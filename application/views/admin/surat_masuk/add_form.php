5f58eac9ef79e69f1806517343e81854840acae0
SaadatuzZuhriyah <saadah1804@gmail.com>
2023-06-24 21:41:00 +0700

    Praktikum Pertemuan 3

--- application/views/admin/surat_masuk/add_form.php
+++ application/views/admin/surat_masuk/add_form.php
@@ -7,35 +7,36 @@
         </ol>
         <div class="card mb-4">
             <div class="card-body">
-                <form action="<?php echo site_url('admin/user/edit') ?>" method="post">
+                <form action="<?php echo site_url('admin/user/save') ?>" method="post">
                     <div class="mb-3">
                         <label for="username">USERNAME <code>*</code></label>
-                        <input class="form-control" type="hidden" name="id" value="<?= $user->id; ?>" required />
-                        <input class="form-control <?php echo form_error('username') ? 'is-invalid' : '' ?>" type="text" name="username" value="<?= $user->username; ?>" placeholder="USERNAME" required />
+                        <input class="form-control <?php echo form_error('username') ? 'is-invalid' : '' ?>" type="text" name="username" placeholder="USERNAME" required />
                         <div class="invalid-feedback">
                             <?php echo form_error('username') ?>
                         </div>
                     </div>
                     <div class="mb-3">
                         <label for="full_name">FULL NAME <code>*</code></label>
-                        <input class="form-control" type="text" name="full_name" value="<?= $user->full_name; ?>" placeholder="FULL NAME" required />
+                        <input class="form-control" type="text" name="full_name" nplaceholder="FULL NAME" required />
                     </div>
                     <div class="mb-3">
                         <label for="phone">PHONE</label>
-                        <input class="form-control" type="text" name="phone" value="<?= $user->phone; ?>" placeholder="PHONE" required />
+                        <input class="form-control" type="number" name="phone" placeholder="PHONE" required />
                     </div>
                     <div class="mb-3">
                         <label for="email">EMAIL</label>
-                        <input class="form-control" type="email" name="email" value="<?= $user->email; ?>" placeholder="EMAIL" required />
+                        <input class="form-control" type="text" name="email" placeholder="EMAIL" required />
+                    </div>
+                    <div class="mb-3">
+                        <label class="control-label col-md-3 col-sm-3 col-xs-12">PASSWORD</label>
+                        <input type="password" class="form-control" id="password" name="password" autocomplete="off" placeholder="PASSWORD" required>
                     </div>
                     <div class="mb-3">
                         <label for="role">Role</label>
                         <select class="form-select" id="role" name="role" required>
-                            <?php if ($user->role == 'admin') { ?>
-                                <option value="admin" selected>ADMIN</option>
-                            <?php } else { ?>
-                                <option value="sekretaris" selected>SEKRETARIS</option>
-                            <?php } ?>
+                            <option selected>Choose...</option>
+                            <option value="admin">ADMIN</option>
+                            <option value="sekretaris">SEKRETARIS</option>
                         </select>
                     </div>
                     <button class="btn btn-primary" type="submit"><i class="fas fa-plus"></i> Save</button>
